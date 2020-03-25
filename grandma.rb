d6a4e55c859aa604adee856b3c16b996973978a6
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-03-25 03:12:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,13 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(arg)
+  response = ""
+  if arg != arg.upcase
+    response = "HUH?! SPEAK UP, SONNY!"
+  elsif arg == arg.upcase
+    response = "NO, NOT SINCE 1938!"
+    
+  
+end
